63104ad485a8c2084b185b5cf4f78a4d59f1b6e4
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-11-23 09:07:28 +0100

    Add hello world endpoint

--- src/main/java/com/example/microservicedocker/MicroserviceDockerApplication.java
+++ src/main/java/com/example/microservicedocker/MicroserviceDockerApplication.java
@@ -2,12 +2,19 @@ package com.example.microservicedocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MicroserviceDockerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceDockerApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 }
