a8da81983b7be036a1d1821fde8b3d7a4efa8b44
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-11-23 08:58:39 +0100

    Initial commit

---
+++ src/main/java/com/example/microservicedocker/MicroserviceDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDockerApplication.class, args);
+    }
+
+}
