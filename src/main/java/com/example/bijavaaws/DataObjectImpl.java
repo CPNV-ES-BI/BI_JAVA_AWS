d4bf3ac1366c7e6c92547c73eba80b380f663681
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-12-07 10:19:21 +0100

    Add S3 dependency

--- src/main/java/com/example/bijavaaws/DataObjectImpl.java
+++ src/main/java/com/example/bijavaaws/DataObjectImpl.java
@@ -1,6 +1,15 @@
 package com.example.bijavaaws;
 
+import software.amazon.awssdk.services.s3.S3Client;
+
 public class DataObjectImpl implements DataObject {
+
+    private final S3Client s3;
+
+    public DataObjectImpl() {
+        s3 = S3Client.create();
+    }
+
     @Override
     public boolean doesExists(Object obj) {
         throw new UnsupportedOperationException("Not implemented yet");
