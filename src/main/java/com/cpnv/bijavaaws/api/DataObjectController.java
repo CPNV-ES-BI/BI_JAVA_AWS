28a5baba15d09eee7eaf5e6890472e00cca73dea
Anthony <bouillant.anthony@gmail.com>
2023-01-31 14:56:08 +0100

    Add Swagger UI

--- src/main/java/com/cpnv/bijavaaws/api/DataObjectController.java
+++ src/main/java/com/cpnv/bijavaaws/api/DataObjectController.java
@@ -1,6 +1,8 @@
 package com.cpnv.bijavaaws.api;
 
 import com.cpnv.bijavaaws.dataobject.DataObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URL;
+
+@Api("DataObject API REST")
 
 @RestController
 @RequestMapping("/api")
@@ -20,11 +26,12 @@ public class DataObjectController {
         this.dataObject = dataObject;
     }
 
+    @ApiOperation(value = "Create an object in the S3 bucket", response = String.class)
     @PostMapping("/objects")
     public ResponseEntity<String> createObject(
             @RequestParam("file") MultipartFile file,
             @RequestParam(name = "key", required = false) String key
-    ){
+    ) {
         if (key == null || key.isEmpty()) {
             key = file.getOriginalFilename();
         }
@@ -32,6 +39,7 @@ public class DataObjectController {
         return ResponseEntity.status(HttpStatus.CREATED).body("Object created successfully");
     }
 
+    @ApiOperation(value = "Download an object from the S3 bucket", response = byte[].class)
     @GetMapping("/objects/{objectKey}")
     public ResponseEntity<byte[]> downloadObject(@PathVariable String objectKey) {
         return ResponseEntity.ok()
@@ -40,15 +48,16 @@ public class DataObjectController {
                 .body(dataObject.downloadObject(objectKey));
     }
 
+    @ApiOperation(value = "Delete an object from the S3 bucket", response = String.class)
     @DeleteMapping("/objects/{objectKey}")
     public ResponseEntity<String> deleteObject(@PathVariable String objectKey, @RequestParam(name = "recursive", required = false) boolean recursive) {
-
         dataObject.deleteObject(objectKey, recursive);
         return ResponseEntity.status(HttpStatus.OK).body("Object deleted successfully");
     }
 
+    @ApiOperation(value = "Publish an object from the S3 bucket", response = URL.class)
     @PatchMapping("/objects/{objectKey}")
-    public ResponseEntity<String> publishObject(@PathVariable String objectKey) {
-        return ResponseEntity.status(HttpStatus.OK).body("Object published successfully: " + dataObject.publishObject(objectKey));
+    public ResponseEntity<URL> publishObject(@PathVariable String objectKey) {
+        return ResponseEntity.status(HttpStatus.OK).body(dataObject.publishObject(objectKey));
     }
 }
