d423dffbb3cff30ee17a59477e5898babb25e2ce
Anthony <bouillant.anthony@gmail.com>
2023-02-07 00:38:32 +0100

    Add Content-Type validation

--- src/main/java/com/cpnv/bijavaaws/controller/DataObjectController.java
+++ src/main/java/com/cpnv/bijavaaws/controller/DataObjectController.java
@@ -1,101 +1,86 @@
 package com.cpnv.bijavaaws.controller;
 
-import com.cpnv.bijavaaws.exceptions.ObjectAlreadyExistsException;
-import com.cpnv.bijavaaws.exceptions.ObjectNotFoundException;
 import com.cpnv.bijavaaws.service.DataObject;
-import io.swagger.annotations.*;
-import org.apache.tika.Tika;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tika.mime.MimeType;
+import org.apache.tika.mime.MimeTypeException;
+import org.apache.tika.mime.MimeTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.net.URL;
 
 @Api("DataObject API REST")
 
 @RestController
+@Validated
 @RequestMapping("/api")
 public class DataObjectController {
 
     @Autowired
     private final DataObject dataObject;
 
-    @Autowired
-    private Tika tika;
-
     public DataObjectController(DataObject dataObject) {
         this.dataObject = dataObject;
     }
 
     @ApiOperation(value = "Create an object in the S3 bucket")
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Object created successfully"),
-            @ApiResponse(code = 409, message = "Object already exists", response = ObjectAlreadyExistsException.class)
-    })
     @PostMapping("/objects")
     public ResponseEntity<String> createObject(
-            @ApiParam(value = "File to upload", required = true, type = "file")
-            @RequestParam("file") MultipartFile file,
-            @ApiParam(value = "Value of the object key")
-            @RequestParam(name = "key") String key
+            @RequestParam("file") @NotNull MultipartFile file,
+            @RequestParam(name = "key") @NotEmpty @NotBlank @NotNull String key
     ) {
-        dataObject.createObject(file, key + "." + tika.detect(file.getContentType()));
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        dataObject.createObject(file, key + "." + extension);
         return ResponseEntity.status(HttpStatus.CREATED).body("Object created successfully");
     }
 
     @ApiOperation(value = "Download an object from the S3 bucket")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Object downloaded successfully", responseHeaders = {
-                    @ResponseHeader(name = "Content-Type", description = "The content type of the object", response = String.class),
-                    @ResponseHeader(name = "Content-Disposition", description = "The content disposition of the object", response = String.class)}),
-            @ApiResponse(code = 404, message = "Object not found", response = ObjectNotFoundException.class)})
     @GetMapping("/objects/{key}")
     public ResponseEntity<byte[]> downloadObject(
-            @ApiParam(value = "Value of the object key", required = true) @PathVariable String key,
-            @RequestHeader(HttpHeaders.CONTENT_TYPE) String contentType
-    ) {
+            @PathVariable String key,
+            @RequestHeader(HttpHeaders.CONTENT_TYPE) @NotNull @NotEmpty String contentType
+    ) throws MimeTypeException {
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + this.getFileWithExtension(key, contentType))
-                .body(dataObject.downloadObject(key));
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + this.getFullFileName(key, contentType))
+                .body(dataObject.downloadObject(this.getFullFileName(key, contentType)));
     }
 
     @ApiOperation(value = "Delete an object from the S3 bucket")
-    @ApiResponses(value = {
-            @ApiResponse(code = 204, message = "Object deleted successfully"),
-            @ApiResponse(code = 404, message = "Object not found", response = ObjectNotFoundException.class)
-    })
     @DeleteMapping("/objects/{key}")
     public ResponseEntity<String> deleteObject(
-            @ApiParam(value = "Value of the object key")
             @PathVariable String key,
-            @ApiParam(value = "If true, the object will be deleted recursively. If false, the object will be deleted only if it is empty.", required = false)
             @RequestParam(name = "recursive", required = false) boolean recursive,
-            @RequestHeader(HttpHeaders.CONTENT_TYPE) String contentType
-    ) {
-        dataObject.deleteObject(this.getFileWithExtension(key, contentType), recursive);
+            @RequestHeader(HttpHeaders.CONTENT_TYPE) @NotNull @NotEmpty String contentType
+    ) throws MimeTypeException {
+        dataObject.deleteObject(this.getFullFileName(key, contentType), recursive);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Object deleted successfully");
     }
 
     @ApiOperation(value = "Publish an object from the S3 bucket")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Object published successfully", response = URL.class, responseHeaders = {@ResponseHeader(name = "Location", description = "URL of the published object", response = URL.class)}),
-            @ApiResponse(code = 404, message = "Object not found", response = ObjectNotFoundException.class)
-    })
     @PatchMapping("/objects/{key}/publish")
     public ResponseEntity<URL> publishObject(
             @ApiParam(value = "Value of the object key") @PathVariable String key,
-            @RequestHeader(HttpHeaders.CONTENT_TYPE) String contentType
-    ) {
-        return ResponseEntity.status(HttpStatus.OK).body(dataObject.publishObject(this.getFileWithExtension(key, contentType)));
+            @RequestHeader(HttpHeaders.CONTENT_TYPE) @NotNull @NotEmpty String contentType
+    ) throws MimeTypeException {
+        return ResponseEntity.status(HttpStatus.OK).body(dataObject.publishObject(this.getFullFileName(key, contentType)));
     }
 
-    private String getFileWithExtension(String key, String contentType) {
-
-        return key + "." + tika.detect(contentType);
+    private String getFullFileName(String key, String contentType) throws MimeTypeException {
+        MimeType mimeType = MimeTypes.getDefaultMimeTypes().forName(contentType);
+        return key + mimeType.getExtension();
     }
 }
--- src/main/java/com/cpnv/bijavaaws/controller/FallbackController.java
+++ src/main/java/com/cpnv/bijavaaws/controller/FallbackController.java
@@ -2,11 +2,14 @@ package com.cpnv.bijavaaws.controller;
 
 import com.cpnv.bijavaaws.exceptions.ObjectAlreadyExistsException;
 import com.cpnv.bijavaaws.exceptions.ObjectNotFoundException;
+import org.apache.tika.mime.MimeTypeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class FallbackController extends ResponseEntityExceptionHandler {
@@ -20,8 +23,18 @@ public class FallbackController extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleException(ConstraintViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MimeTypeException.class)
+    public ResponseEntity<String> handleException(MimeTypeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
